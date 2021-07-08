867235024267dcaca784ebf0036c82ee39b83d49
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-07-08 16:55:13 -0700

    added some pages

---
+++ pages/projects.tsx
@@ -0,0 +1,7 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+const Projects = ({ Component, pageProps }: AppProps) => {
+  return <div>Projects page</div>;
+};
+export default Projects;
---
+++ pages/timeline.tsx
@@ -0,0 +1,7 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+const Timeline = ({ Component, pageProps }: AppProps) => {
+  return <div>Timeline</div>;
+};
+export default Timeline;
