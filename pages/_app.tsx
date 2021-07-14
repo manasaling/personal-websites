e6aa52efe282d5bc549efa004154e6401df7d7cb
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-07-13 19:22:00 -0700

    idk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+// add bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
