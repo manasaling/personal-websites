45c1ec72d453e5650a4058af932090dee675674a
manasaling <lgmanasa@gmail.com>
2021-07-13 17:19:22 -0700

    added header

---
+++ pages/Projects.tsx
@@ -0,0 +1,3 @@
+export default function Projects() {
+    return <div><p>Projects Page</p></div>;
+  }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.css";
+import NavigationBar from '../components/NavigationBar';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <NavigationBar/>
+    <Component {...pageProps} />
+    </>
+
+  )
+ 
 }
 export default MyApp
