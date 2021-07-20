04332290507c5c2da4f83175a369793e0bf96248
Dylan Lu <dylanelu@gmail.com>
2021-07-19 23:29:38 -0700

    projects almost ready. need to figure out react context state

--- components/Projects/ProjectViewer.tsx
+++ components/Projects/ProjectViewer.tsx
@@ -1,9 +1,17 @@
 import Card from "react-bootstrap/Card";
 
+import { useAppContext } from "../../context/state";
+
 const ProjectViewer = () => {
+  const context = useAppContext();
+
+  const project = context.project;
+
   return (
     <Card style={{ width: "70%" }}>
-      <Card.Title>Hello</Card.Title>
+      <Card.Title>{project.title}</Card.Title>
+      <Card.Body>{project.description}</Card.Body>
+      <Card.Img src={project.image} />
     </Card>
   );
 };
