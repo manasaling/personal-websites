ba3890fa8c66ce8227f441c459382946973a5d45
manasaling <lgmanasa@gmail.com>
2021-07-21 10:07:49 -0700

    home page styling

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,3 +1,17 @@
-export default function Contact() {
-    return <div>Contact Page</div>;
-  }
+// Bootstrap
+import Container from "react-bootstrap/Container";
+
+// Components
+import ProjectIconList from "../components/Projects/ProjectIconList";
+import ProjectViewer from "../components/Projects/ProjectViewer";
+
+export default function Projects() {
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <ProjectIconList />
+        <ProjectViewer />
+      </div>
+    </div>
+  );
+}
--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,3 +1,41 @@
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import styles from './HomePage.module.css'
 export default function HomePage() {
-    return <div><p>Home Page</p></div>;
+    return <div>
+      <div className={styles.homepage}>
+
+        <div className={styles.detail}>
+
+          <div className={styles.textContainer}>
+
+            <div className={styles.name}>
+              <h1>Name</h1>
+            </div>
+
+             <div className={styles.bio}>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+             </div>
+
+            <div className="social-media">
+              <SocialIcon url="https://www.linkedin.com/in/manasa-lingireddy/" />
+              <SocialIcon url="https://github.com/manasaling" />
+              <SocialIcon url="https://www.instagram.com/manasa.ling/" />
+            </div>
+
+          </div>
+
+        
+
+          <div className={styles.profilePicture}>
+            <img src={"./picture.jpg"} alt="profile picture"/>
+          </div>
+
+     
+
+        </div>
+
+      </div>
+    
+      </div>;
   }
