8be35170095146f442be1a54a767fab97db1932b
manasaling <lgmanasa@gmail.com>
2021-07-22 15:58:11 -0700

    changed color of nav bar

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,17 +1,40 @@
-// Bootstrap
-import Container from "react-bootstrap/Container";
-
-// Components
-import ProjectIconList from "../components/Projects/ProjectIconList";
-import ProjectViewer from "../components/Projects/ProjectViewer";
+import {Form, Button, Col} from "react-bootstrap"
+import styles from './Contact.module.css'
 
 export default function Projects() {
   return (
-    <div>
-      <div style={{ display: "flex" }}>
-        <ProjectIconList />
-        <ProjectViewer />
+    <div className={styles.contactForm}>
+      <div className={styles.contactTitle}>
+        <h1>Contact Me</h1>
       </div>
+      <Form>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridFirstName">
+      <Form.Label>First Name</Form.Label>
+      <Form.Control type="first-name" placeholder="First Name" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridLastName">
+      <Form.Label>Last Name</Form.Label>
+      <Form.Control type="last-name" placeholder="Last Name" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridEmail">
+    <Form.Label>Email</Form.Label>
+    <Form.Control placeholder="example@gmail.com" />
+  </Form.Group>
+  
+  <Form.Group controlId="formGridMessage">
+    <Form.Label>Message</Form.Label>
+    <Form.Control placeholder="Enter Message" />
+  </Form.Group>
+
+
+  <Button className={styles.submitButton}  type="submit">
+    Submit
+  </Button>
+</Form>
     </div>
   );
 }
--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import styles from './HomePage.module.css'
+
 export default function HomePage() {
     return <div>
+
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Anton&family=Roboto:wght@300&display=swap');
+</style>
       <div className={styles.homepage}>
 
         <div className={styles.detail}>
@@ -17,7 +24,7 @@ export default function HomePage() {
               <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
              </div>
 
-            <div className="social-media">
+            <div className={styles.socialMediaIcons}>
               <SocialIcon url="https://www.linkedin.com/in/manasa-lingireddy/" />
               <SocialIcon url="https://github.com/manasaling" />
               <SocialIcon url="https://www.instagram.com/manasa.ling/" />
