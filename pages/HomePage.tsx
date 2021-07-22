34a01a482ddf17e6d2409f49bf012dee8d5a8be6
karim-maftoun <kmaftoun@gmail.com>
2021-07-22 15:50:00 -0700

    spaced icons, made the image a circel

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -10,24 +10,28 @@ export default function HomePage() {
           <div className={styles.textContainer}>
 
             <div className={styles.name}>
-              <h1>Name</h1>
+              <h1>Hi my name is ___</h1> 
             </div>
 
              <div className={styles.bio}>
               <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
              </div>
 
-            <div className="social-media">
-              <SocialIcon url="https://www.linkedin.com/in/manasa-lingireddy/" />
-              <SocialIcon url="https://github.com/manasaling" />
-              <SocialIcon url="https://www.instagram.com/manasa.ling/" />
+            <div className={styles["social-media"]}>
+              <SocialIcon className={styles["social-icon"]} url="https://www.linkedin.com/in/manasa-lingireddy/" />
+              <SocialIcon className={styles["social-icon"]} url= "https://github.com/manasaling" />
+              <SocialIcon className={styles["social-icon"]} url="https://www.instagram.com/manasa.ling/" />
+              <SocialIcon className={styles["social-icon"]} url="https://twitter.com/itookstock" />
+              <SocialIcon className={styles["social-icon"]} url="https://www.tiktok.com/@willsmith?lang=en" />
+              <SocialIcon className={styles["social-icon"]} url="https://www.twitch.tv/sykkuno" />
+              <SocialIcon className={styles["social-icon"]} url="https://soundcloud.com/octobersveryown" />
             </div>
 
           </div>
 
         
 
-          <div className={styles.profilePicture}>
+          <div className={styles.cirleimg}>
             <img src={"./picture.jpg"} alt="profile picture"/>
           </div>
 
