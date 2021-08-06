b6593e8c1d22cfaf508613366829593cd3dd1a73
Ethan Keshishian <ehk2000@sbcglobal.net>
2021-08-06 14:40:48 -0700

    Updated global css variables

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -1,21 +1,21 @@
 // Bootstrap
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // Components
-import ProjectIconList from "../components/Projects/ProjectIconList";
-import ProjectViewer from "../components/Projects/ProjectViewer";
+import ProjectIconList from '../components/Projects/ProjectIconList';
+import ProjectViewer from '../components/Projects/ProjectViewer';
 
 export default function Projects() {
   return (
     <div>
-      <Container fluid style={{ height: "90vh" }}>
-        <Row style={{ maxHeight: "100%" }}>
-          <Col sm={12} lg={2} style={{ maxHeight: "100%" }}>
+      <Container fluid style={{ height: 'var(--page-height)' }}>
+        <Row style={{ maxHeight: '100%' }}>
+          <Col sm={12} lg={2} style={{ maxHeight: '100%' }}>
             <ProjectIconList />
           </Col>
-          <Col sm={12} lg={10} style={{ maxHeight: "100%" }}>
+          <Col sm={12} lg={10} style={{ maxHeight: '100%' }}>
             <ProjectViewer />
           </Col>
         </Row>
